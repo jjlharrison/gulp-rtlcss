ef6a8de7a3cc3d64af7517f5dde5f018c344e04c
James Harrison <james.harrison@cognitran.com>
2018-01-10 22:16:15 +0000

    Add support for gulp-sourcemaps
    resolves #9

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
     var PluginError = require('plugin-error'),
         through = require('through2'),
         rtlcss = require('rtlcss'),
-        configLoader = require('rtlcss/lib/config-loader');
+        configLoader = require('rtlcss/lib/config-loader'),
+        applySourceMap = require('vinyl-sourcemaps-apply');
 
     module.exports = function (config) {
         return through.obj(function (file, enc, cb) {
+            var result;
             if (file.isNull()) {
                 this.push(file);
                 return cb();
@@ -21,7 +23,16 @@
                 config = configLoader.load(null, file.cwd);
             }
 
-            file.contents = new Buffer(rtlcss.configure(config).process(file.contents.toString()).css);
+            result = rtlcss.configure(config).process(file.contents.toString(), {
+                prev: file.sourceMap
+            });
+
+            file.contents = Buffer.from(result.css);
+
+            if (file.map) {
+                applySourceMap(file, result.map);
+            }
+
             this.push(file);
             cb();
         });
